144a5f7dd5cb3a85507560911c986d2e0997de89
apple-x-co <apple.x.co@gmail.com>
2023-01-12 16:25:50 +0900

    add text_demo

--- src/main.zig
+++ src/main.zig
@@ -112,7 +112,7 @@ test "text_demo" {
     const allocator = std.testing.allocator;
 
     const sample_text = "abcdefgABCDEFG123!#$%&+-@?";
-    // const sample_text2 = "The quick brown fox jumps over the lazy dog.";
+    const sample_text2 = "The quick brown fox jumps over the lazy dog.";
 
     const page_title = "Text Demo";
     var pdf = c.HPDF_New(null, null);
@@ -232,6 +232,119 @@ test "text_demo" {
     _ = c.HPDF_Page_TextOut(page, 60, ypos, "ABCabc123");
     _ = c.HPDF_Page_EndText(page);
 
+    // PDF_STROKE
+    show_description(page, 60, ypos - 50, "RenderingMode=PDF_STROKE");
+    _ = c.HPDF_Page_SetTextRenderingMode(page, c.HPDF_STROKE);
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_TextOut(page, 60, ypos - 50, "ABCabc123");
+    _ = c.HPDF_Page_EndText(page);
+
+    // PDF_FILL_THEN_STROKE
+    show_description(page, 60, ypos - 100, "RenderingMode=PDF_FILL_THEN_STROKE");
+    _ = c.HPDF_Page_SetTextRenderingMode(page, c.HPDF_FILL_THEN_STROKE);
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_TextOut(page, 60, ypos - 100, "ABCabc123");
+    _ = c.HPDF_Page_EndText(page);
+
+    // PDF_FILL_CLIPPING
+    show_description(page, 60, ypos - 150, "RenderingMode=PDF_FILL_CLIPPING");
+    _ = c.HPDF_Page_GSave(page);
+    _ = c.HPDF_Page_SetTextRenderingMode(page, c.HPDF_FILL_CLIPPING);
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_TextOut(page, 60, ypos - 150, "ABCabc123");
+    _ = c.HPDF_Page_EndText(page);
+    show_stripe_pattern(page, 60, ypos - 150);
+    _ = c.HPDF_Page_GRestore(page);
+
+    // PDF_STROKE_CLIPPING
+    show_description(page, 60, ypos - 200, "RenderingMode=PDF_STROKE_CLIPPING");
+    _ = c.HPDF_Page_GSave(page);
+    _ = c.HPDF_Page_SetTextRenderingMode(page, c.HPDF_STROKE_CLIPPING);
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_TextOut(page, 60, ypos - 200, "ABCabc123");
+    _ = c.HPDF_Page_EndText(page);
+    show_stripe_pattern(page, 60, ypos - 200);
+    _ = c.HPDF_Page_GRestore(page);
+
+    // /* PDF_FILL_STROKE_CLIPPING */
+    show_description(page, 60, ypos - 250, "RenderingMode=PDF_FILL_STROKE_CLIPPING");
+    _ = c.HPDF_Page_GSave(page);
+    _ = c.HPDF_Page_SetTextRenderingMode(page, c.HPDF_FILL_STROKE_CLIPPING);
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_TextOut(page, 60, ypos - 250, "ABCabc123");
+    _ = c.HPDF_Page_EndText(page);
+    show_stripe_pattern(page, 60, ypos - 250);
+    _ = c.HPDF_Page_GRestore(page);
+
+    // Reset text attributes */
+    _ = c.HPDF_Page_SetTextRenderingMode(page, c.HPDF_FILL);
+    _ = c.HPDF_Page_SetRGBFill(page, 0, 0, 0);
+    _ = c.HPDF_Page_SetFontAndSize(page, font, 30);
+
+    // Rotating text
+    var angle1: f32 = 30; // A rotation of 30 degrees.
+    var rad1: f32 = angle1 / 180.0 * 3.141592; // Calculate the radian value.
+
+    show_description(page, 320, ypos - 60, "Rotating text");
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_SetTextMatrix(page, @cos(rad1), @sin(rad1), -@sin(rad1), @cos(rad1), 330, ypos - 60);
+    _ = c.HPDF_Page_ShowText(page, "ABCabc123");
+    _ = c.HPDF_Page_EndText(page);
+
+    // Skewing text.
+    show_description(page, 320, ypos - 120, "Skewing text");
+    _ = c.HPDF_Page_BeginText(page);
+
+    angle1 = 10;
+    var angle2: f32 = 20.0;
+    rad1 = angle1 / 180.0 * 3.141592;
+    var rad2: f32 = angle2 / 180.0 * 3.141592;
+
+    _ = c.HPDF_Page_SetTextMatrix(page, 1, @tan(rad1), @tan(rad2), 1, 320, ypos - 120);
+    _ = c.HPDF_Page_ShowText(page, "ABCabc123");
+    _ = c.HPDF_Page_EndText(page);
+
+    // scaling text (X direction)
+    show_description(page, 320, ypos - 175, "Scaling text (X direction)");
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_SetTextMatrix(page, 1.5, 0, 0, 1, 320, ypos - 175);
+    _ = c.HPDF_Page_ShowText(page, "ABCabc12");
+    _ = c.HPDF_Page_EndText(page);
+
+    // scaling text (Y direction)
+    show_description(page, 320, ypos - 250, "Scaling text (Y direction)");
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_SetTextMatrix(page, 1, 0, 0, 2, 320, ypos - 250);
+    _ = c.HPDF_Page_ShowText(page, "ABCabc123");
+    _ = c.HPDF_Page_EndText(page);
+
+    // char spacing, word spacing
+    show_description(page, 60, 140, "char-spacing 0");
+    show_description(page, 60, 100, "char-spacing 1.5");
+    show_description(page, 60, 60, "char-spacing 1.5, word-spacing 2.5");
+
+    _ = c.HPDF_Page_SetFontAndSize(page, font, 20);
+    _ = c.HPDF_Page_SetRGBFill(page, 0.1, 0.3, 0.1);
+
+    // char-spacing 0
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_TextOut(page, 60, 140, sample_text2);
+    _ = c.HPDF_Page_EndText(page);
+
+    // char-spacing 1.5
+    _ = c.HPDF_Page_SetCharSpace(page, 1.5);
+
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_TextOut(page, 60, 100, sample_text2);
+    _ = c.HPDF_Page_EndText(page);
+
+    // char-spacing 1.5, word-spacing 3.5
+    _ = c.HPDF_Page_SetWordSpace(page, 2.5);
+
+    _ = c.HPDF_Page_BeginText(page);
+    _ = c.HPDF_Page_TextOut(page, 60, 60, sample_text2);
+    _ = c.HPDF_Page_EndText(page);
+
     // save the document to a file
     _ = c.HPDF_SaveToFile(pdf, "./output/text_demo.pdf");
 }
@@ -250,4 +363,18 @@ fn show_description(page: c.HPDF_Page, x: c.HPDF_REAL, y: c.HPDF_REAL, text: []c
 
     _ = c.HPDF_Page_SetFontAndSize(page, font, fsize);
     _ = c.HPDF_Page_SetRGBFill(page, color.r, color.g, color.b);
+}
+
+fn show_stripe_pattern(page: c.HPDF_Page, x: c.HPDF_REAL, y: c.HPDF_REAL) void {
+    var iy: c.HPDF_REAL = 0;
+
+    while (iy < 50) : (iy += 3) {
+        _ = c.HPDF_Page_SetRGBStroke(page, 0.0, 0.0, 0.5);
+        _ = c.HPDF_Page_SetLineWidth(page, 1);
+        _ = c.HPDF_Page_MoveTo(page, x, y + iy);
+        _ = c.HPDF_Page_LineTo(page, x + c.HPDF_Page_TextWidth(page, "ABCabc123"), y + iy);
+        _ = c.HPDF_Page_Stroke(page);
+    }
+
+    _ = c.HPDF_Page_SetLineWidth(page, 2.5);
 }
