f1e564db9631250e45c8dd8a30a864649c884d05
apple-x-co <apple.x.co@gmail.com>
2023-01-13 13:23:11 +0900

    chore: grid

--- src/main.zig
+++ src/main.zig
@@ -128,7 +128,7 @@ test "text_demo" {
     var page = c.HPDF_AddPage(pdf);
 
     // draw grid to the page
-    // print_grid  (pdf, page);
+    print_grid(pdf, page);
 
     // print the title of the page (with positioning center).
     _ = c.HPDF_Page_SetFontAndSize(page, font, 24);
@@ -349,6 +349,108 @@ test "text_demo" {
     _ = c.HPDF_SaveToFile(pdf, "./output/text_demo.pdf");
 }
 
+fn print_grid(pdf: c.HPDF_Doc, page: c.HPDF_Page) void {
+    const height: f32 = c.HPDF_Page_GetHeight(page);
+    const width: f32 = c.HPDF_Page_GetWidth(page);
+    const font = c.HPDF_GetFont(pdf, "Helvetica", null);
+    var x: u64 = 0;
+    var y: u64 = 0;
+
+    _ = c.HPDF_Page_SetFontAndSize(page, font, 5);
+    _ = c.HPDF_Page_SetGrayFill(page, 0.5);
+    _ = c.HPDF_Page_SetGrayStroke(page, 0.8);
+
+    // Draw horizontal lines
+    y = 0;
+    while (@intToFloat(f32, y) < height) : (y += 5) {
+        if (y % 10 == 0) {
+            _ = c.HPDF_Page_SetLineWidth(page, 0.5);
+        } else {
+            if (c.HPDF_Page_GetLineWidth(page) != 0.25)
+                _ = c.HPDF_Page_SetLineWidth(page, 0.25);
+        }
+
+        _ = c.HPDF_Page_MoveTo(page, 0, @intToFloat(f32, y));
+        _ = c.HPDF_Page_LineTo(page, width, @intToFloat(f32, y));
+        _ = c.HPDF_Page_Stroke(page);
+
+        if ((y % 10 == 0) and (y > 0)) {
+            _ = c.HPDF_Page_SetGrayStroke(page, 0.5);
+
+            _ = c.HPDF_Page_MoveTo(page, 0, @intToFloat(f32, y));
+            _ = c.HPDF_Page_LineTo(page, 5, @intToFloat(f32, y));
+            _ = c.HPDF_Page_Stroke(page);
+
+            _ = c.HPDF_Page_SetGrayStroke(page, 0.8);
+        }
+    }
+
+    // Draw vertical lines
+    x = 0;
+    while (@intToFloat(f32, x) < width) : (x += 5) {
+        if (x % 10 == 0) {
+            _ = c.HPDF_Page_SetLineWidth(page, 0.5);
+        } else {
+            if (c.HPDF_Page_GetLineWidth(page) != 0.25)
+                _ = c.HPDF_Page_SetLineWidth(page, 0.25);
+        }
+
+        _ = c.HPDF_Page_MoveTo(page, @intToFloat(f32, x), 0);
+        _ = c.HPDF_Page_LineTo(page, @intToFloat(f32, x), height);
+        _ = c.HPDF_Page_Stroke(page);
+
+        if ((x % 50) == 0 and (x > 0)) {
+            _ = c.HPDF_Page_SetGrayStroke(page, 0.5);
+
+            _ = c.HPDF_Page_MoveTo(page, @intToFloat(f32, x), 0);
+            _ = c.HPDF_Page_LineTo(page, @intToFloat(f32, x), 5);
+            _ = c.HPDF_Page_Stroke(page);
+
+            _ = c.HPDF_Page_MoveTo(page, @intToFloat(f32, x), height);
+            _ = c.HPDF_Page_LineTo(page, @intToFloat(f32, x), height - 5);
+            _ = c.HPDF_Page_Stroke(page);
+
+            _ = c.HPDF_Page_SetGrayStroke(page, 0.8);
+        }
+    }
+
+    // Draw horizontal text
+    y = 0;
+    while (@intToFloat(f32, y) < height) : (y += 5) {
+        if ((y % 10) == 0 and (y > 0)) {
+            var buf: [12]u8 = undefined;
+
+            _ = c.HPDF_Page_BeginText(page);
+            _ = c.HPDF_Page_MoveTextPos(page, 5, @intToFloat(f32, y - 2));
+            var text = std.fmt.bufPrintZ(&buf, "{}", .{y}) catch return;
+            _ = c.HPDF_Page_ShowText(page, text.ptr);
+            _ = c.HPDF_Page_EndText(page);
+        }
+    }
+
+    // Draw vertical text
+    x = 0;
+    while (@intToFloat(f32, x) < width) : (x += 5) {
+        if ((x % 50) == 0 and (x > 0)) {
+            var buf: [12]u8 = undefined;
+
+            _ = c.HPDF_Page_BeginText(page);
+            _ = c.HPDF_Page_MoveTextPos(page, @intToFloat(f32, x), 5);
+            var text = std.fmt.bufPrintZ(&buf, "{}", .{x}) catch return;
+            _ = c.HPDF_Page_ShowText(page, text.ptr);
+            _ = c.HPDF_Page_EndText(page);
+
+            _ = c.HPDF_Page_BeginText(page);
+            _ = c.HPDF_Page_MoveTextPos(page, @intToFloat(f32, x), height - 10);
+            _ = c.HPDF_Page_ShowText(page, text.ptr);
+            _ = c.HPDF_Page_EndText(page);
+        }
+    }
+
+    _ = c.HPDF_Page_SetGrayFill(page, 0);
+    _ = c.HPDF_Page_SetGrayStroke(page, 0);
+}
+
 fn show_description(page: c.HPDF_Page, x: c.HPDF_REAL, y: c.HPDF_REAL, text: []const u8) void {
     const fsize = c.HPDF_Page_GetCurrentFontSize(page);
     const font = c.HPDF_Page_GetCurrentFont(page);
@@ -393,7 +495,8 @@ test "text_demo2" {
     var page = c.HPDF_AddPage(pdf);
     _ = c.HPDF_Page_SetSize(page, c.HPDF_PAGE_SIZE_A5, c.HPDF_PAGE_PORTRAIT);
 
-    // const height = c.HPDF_Page_GetHeight(page);
+    // draw grid to the page
+    print_grid(pdf, page);
 
     var font = c.HPDF_GetFont(pdf, "Helvetica", null);
     _ = c.HPDF_Page_SetTextLeading(page, 20);
